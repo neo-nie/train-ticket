02067f02f16bb237cf075e53b79cb991595af4e7
聂红亮 <niehongliang@gmail.com>
2013-10-15 10:15:01 +0800

    update search module

--- js/search.js
+++ js/search.js
@@ -16,6 +16,26 @@ avalon.define("search", function (vm) {
 	vm.IsDongChe = true;
 	vm.IsGaoTie = true;
 
+	//候选城市列表，默认显示热门城市。
+	//如果在城市输入框中输入了关键字，则显示过滤后的结果
+	vm.CandidateCities = [
+        "上海", "北京", "广州", "昆明",
+        "西安", "成都", "深圳", "厦门",
+        "南京", "重庆", "杭州", "大连",
+        "长沙", "海口", "青岛", "沈阳",
+        "三亚", "济南", "武汉", "郑州",
+        "贵阳", "南宁", "福州", "天津",
+    ];
+
+    /**
+     * 交换城市起点与终点
+    */
+    vm.ExchangeCity = function (e) {
+            var tmp = vm.FromCity;
+            vm.FromCity = vm.ToCity;
+            vm.ToCity = tmp;
+        };
+
 	/**
 	 * 校验查询条件
 	*/
